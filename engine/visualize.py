43370f2acaa39d63225214b60b84f8c01f839af1
sam tenka <samtenka@umich.edu>
2016-11-21 16:29:18 -0500

    document

--- engine/visualize.py
+++ engine/visualize.py
@@ -10,23 +10,40 @@ from data_scrape.fetch_data import fetch_Xy
 import numpy as np
 import matplotlib.pyplot as plt
 from random import randrange
+from collections import Counter
 
 def rescale_colors(Xs):
-    ''' Channelwise affine transform into range [0, 1]. '''
+    ''' Perform channelwise affine transform into range [0, 1]. '''
     maxes = np.amax(Xs, axis=(0, 1, 2))
     mins = np.amin(Xs, axis=(0, 1, 2))
     for i in range(3):
         Xs[:,:,:,i] = (Xs[:,:,:,i]-mins[i]) / (maxes[i]-mins[i])
     return Xs
 
+def get_stats(ys):
+    C = Counter(ys)
+    for i in range(2):
+        print('I see %d instances of label %d.' % (C[i], i))
+
 def visualize():
+    ''' Interactively display images of specified class.
+        
+        User enters '0', '1', or ''; a random instance of
+        class 0, 1, or any class is then displayed. Close
+        the popup window to continue, and exit via 'exit'.
+    '''
     X_vis, y_vis = fetch_Xy('VIS') 
     X_vis = rescale_colors(X_vis)
+    get_stats(y_vis)
     for command in user_input_iterator():
         while True:
             index = randrange(len(X_vis))
             img, label = X_vis[index], y_vis[index]
-            if (not command) or (label == int(command)): break
-        print('label: %d (index %d)' % (label, index))
+            if (command not in '01') or (label == int(command)): break
+        print('Image %d has label %d' % (index, label))
+        plt.title('Image %d has label %d' % (index, label))
         plt.imshow(img) 
         plt.show()
+
+if __name__=='__main__':
+    visualize()
