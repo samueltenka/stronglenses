26eae74a8eeba59c261bf8a492653a8fc0c82e7e
sam tenka <samtenka@umich.edu>
2016-11-30 23:04:58 -0500

    roc

--- engine/view_curves.py
+++ engine/view_curves.py
@@ -3,7 +3,7 @@
     descr: Compute and view yield, conf, history curves (on test-set).
     usage: Type
             python -m engine.view_curves
-        Type 'yield', 'conf' to switch between modes.
+        Type 'yield', 'conf', or 'roc' to switch between modes.
         Type a space-separated list of names (e.g. 'MLP SHALLOW_RES')
         to produce plots. 
 '''
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import fftconvolve
 
 X, y = fetch_Xy('TEST')
+classes = [0, 1] # we assume binary classification
 
 def blur(array, sigma=1.0, N=6):
     L = int(1+N*sigma)
@@ -26,17 +27,47 @@ def blur(array, sigma=1.0, N=6):
     return fftconvolve(array, gaussian)[L:-L]
 
 @memoize
-def get_cums(model_nm, bins=100, eps=1e-4):
-    ''' Return domain [0.5, 1.0] of conf, cumulative counts of correct predictions
-        on sets of given min conf, and cumulative counts of all predictions on
-        those sets.
-    '''
+def get_prediction(model_nm):
     model, _ = fetch_model(model_nm)
-    predictions = model.predict(X, batch_size=30, verbose=1)[:,0]
+    return model.predict(X, batch_size=30, verbose=1)[:,0]
+
+def get_preds_by_class(model_nm):
+    ''' Return {c:[predicted chance of c for data in class c]
+                for c in classes}
+        Values are sorted.
+    '''
+    preds = get_prediction(model_nm)
+    preds_by_class = {c:[] for c in classes}
+    for p, label in zip(preds, y):
+        if label==0: p = 1-p  
+        preds_by_class[label].append(p)
+    for c in classes:
+        preds_by_class[c].sort()
+        preds_by_class[c] = np.array(preds_by_class[c])
+    return preds_by_class
+
+def get_ROC(model_nm, nb_thresh=100):
+    ''' Return (selectivity, sensitivity)
+
+        Each array has length (nb_thresh+1):
+    '''
+    preds_by_class = get_preds_by_class(model_nm)
+    threshs = np.arange(0.0, 1.0+1.0/nb_thresh, 1.0/nb_thresh)
+    threshs = {0: 1-threshs, 1:threshs}
+    recalls_by_class = {c: 1.0 - (np.searchsorted(preds, threshs[c]).astype(float) / len(preds))
+                        for c, preds in preds_by_class.items()}
+    return tuple(recalls_by_class[c] for c in classes)
+
+def get_cums(model_nm, bins=100, eps=1e-4):
+    ''' Return domain [0.5, 1.0] of conf, cumulative counts of correct
+        predictions on sets of given min conf, and cumulative counts of
+        all predictions on those sets.
+    '''
+    preds = get_prediction(model_nm)
     confidences = np.arange(0.5, 1.0 + 1.0/(2*bins), 1.0/(2*bins))
     correct_histogram = np.zeros(bins + 1) + eps
     total_histogram = np.zeros(bins + 1) + eps
-    for p, label in zip(predictions, y):
+    for p, label in zip(preds, y):
         c = abs(p-0.5)
         index = int(c*bins) 
         total_histogram[index] += 1.0
@@ -47,6 +78,12 @@ def get_cums(model_nm, bins=100, eps=1e-4):
     correct_cum = blur(correct_cum, sigma=bins/40)
     total_cum = blur(total_cum, sigma=bins/40)
     return confidences, correct_cum, total_cum 
+
+def roc_curve(model_nm):
+    ''' A `receiver-operating characteristic curve`
+        plots sensitivity vs selectivity.
+    '''
+    return get_ROC(model_nm)
 
 def conf_curve(model_nm):
     ''' A `confidence curve` plots accuracy vs min conf '''
@@ -64,10 +101,13 @@ def yield_curve(model_nm):
     i = np.argmax(total_cum)
     return (total_cum/max(total_cum))[i:], (correct_cum/total_cum)[i:]
 
+curve_getters_by_mode = {
+    'yield': yield_curve,
+    'conf': conf_curve,
+    'roc': roc_curve
+}
 def compute_curve(model_nm, mode): 
-    return {'yield':yield_curve,
-            'conf':conf_curve
-            }[mode](model_nm)
+    return curve_getters_by_mode[mode](model_nm)
 
 def view_curves():
     ''' Interactively save and display yield or confidence curves
@@ -77,26 +117,33 @@ def view_curves():
     '''
     mode = 'yield'  
     for command in user_input_iterator():
-        if command in ('yield', 'conf'):
+        if command in curve_getters_by_mode:
             mode = command
             print('switched to `%s` mode' % mode)
             continue
-        nms = command.split(' ')
-        for nm in nms:
+
+        model_nms = command.split() 
+        for nm in model_nms:
             color = get('MODEL.%s.PLOTCOLOR' % nm)
-            domain, accuracies = compute_curve(nm, mode)
-            plt.plot(domain, accuracies, label='%s' % nm,
+            xvals, yvals = compute_curve(nm, mode)
+            plt.plot(xvals, yvals, label='%s' % nm,
                      color=color, ls='-', lw=2.0)
-        if mode=='conf':
-            plt.plot(domain, domain, label='truth',
-                         color='k', ls='-', lw=1.0)
-            plt.title('Confidence curves of %s' % ', '.join(nms))
-            plt.gca().set_xlabel('Confidence threshold C')
-            plt.gca().set_ylabel('Accuracy on data on which model is at least C confident')
-        else:
-            plt.title('Yield curves of %s' % ', '.join(nms))
+        if mode=='yield':
+            plt.title('Yield curves of %s' % ', '.join(model_nms))
             plt.gca().set_xlabel('Fraction F of data')
             plt.gca().set_ylabel('Accuracy on top F of data, by confidence')
+        elif mode=='conf':
+            plt.plot(xvals, xvals, label='truth',
+                     color='k', ls='-', lw=1.0)
+            plt.title('Confidence curves of %s' % ', '.join(model_nms))
+            plt.gca().set_xlabel('Confidence threshold C')
+            plt.gca().set_ylabel('Accuracy on data on which model is at least C confident')
+        elif mode=='roc':
+            plt.title('(Reflected) ROC curves of %s' % ', '.join(model_nms)) 
+            plt.gca().set_xlabel('Selectivity p(guess = - | truth = -)')
+            plt.gca().set_ylabel('Sensitivity p(guess = + | truth = +)')
+        else:
+            assert(False)
         plt.legend(loc='best')
         fig_nm = command.replace(' ', '_vs_') + '.%s.png' % mode
         plt.savefig(get('TRAIN.FIGURE_DIR') + '/' + fig_nm)
